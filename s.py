955775f9aa5d72a3d50d614f1e94e91c2476e692
Clayton Dembski <cjdembski@wpi.edu>
2022-02-27 22:52:15 -0500

    scr

---
+++ s.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+import time
+import os
+if __name__ == '__main__':
+    print(sys.argv[1])
+    comp = "https://www.intel.com/content/www/us/en/homepage.html"
+    len(comp)
+
+    r = requests.head("https://www.intel.com/content/www/us/en/download/" + str(sys.argv[1])+"/", allow_redirects=True)
+    time.sleep(3)
+    if(r.url[0:len(comp)] != comp):
+        name = ""+str(sys.argv[1])+".txt"
+        f = open(""+str(sys.argv[1])+".txt", "w")
+        f.write(r.url)
+        f.close()
+        os.system("gsutil cp " + name + " gs://int_valid_webs/" + name)
+    if(r.status_code == 403):
+        name = "FORBIDDEN" + str(sys.argv[1]) + ".txt"
+        f = open("FORBIDDEN" + str(sys.argv[1]) + ".txt", "w")
+        f.close()
+        os.system("gsutil cp " +name + " gs://int_valid_webs/"+name)
---
+++ scrape.py
@@ -0,0 +1,16 @@
+import os
+import time
+import random
+import sys
+if __name__ == '__main__':
+    print(sys.argv[1])
+    splitter = 2000000/20
+    for l in range(997):
+        for k in range(3):
+            for j in range(10):
+                for i in range(10):
+                    os.system("start python3 s.py " + str((3*l+k)*100 + splitter*(int(sys.argv[1])-1)))
+                    time.sleep(random.randrange(0,3))
+                time.sleep(random.randrange(1, 5))
+            time.sleep(random.randrange(15, 20))
+        time.sleep(random.randrange(15, 20))
